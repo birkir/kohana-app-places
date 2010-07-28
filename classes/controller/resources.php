92cfcb14d88fb98f5d9923cd7ec541c581ebfa79
Birkir R Gudjonsson <birkir.gudjonsson@gmail.com>
2010-07-28 13:58:29 +0000

    Added gzip and cache to resources controller

--- classes/controller/resources.php
+++ classes/controller/resources.php
@@ -11,39 +11,70 @@ class Controller_Resources extends Controller {
 
 	public function action_img($file='')
 	{
+		$file = APPPATH.$this->_directory.$this->request->action."/".$file;
 		$this->passthru($file);
 	}
 
 	public function action_css($file='')
 	{
-		$this->passthru($file);
+		$file = APPPATH.$this->_directory.$this->request->action."/".$file;
+		$file = $this->gzip($file);
+		$this->passthru($file, true);
 	}
 
 	public function action_js($file='')
 	{
-		$this->passthru($file);
+		$file = APPPATH.$this->_directory.$this->request->action."/".$file;
+		$file = $this->gzip($file);
+		$this->passthru($file, true);
 	}
 
-	public function passthru($file='')
+	public function passthru($file='', $gzip=false)
 	{
-		$this->file = APPPATH.$this->_directory.$this->request->action."/".$file;
-
-		if (!is_file($this->file))
+		if (!is_file($file))
 		{
-			throw new Kohana_Exception('Image does not exist');
+			throw new Kohana_Exception('File does not exist');
 		}
-
-		$this->request->headers['Content-Type'] = File::mime_by_ext($this->file);
-		$this->request->headers['Content-length'] = filesize($this->file);
-
+		
+		if ($this->request->accept_encoding('gzip') AND $gzip==true)
+		{
+			$this->request->headers['Content-Encoding'] = 'gzip';
+		}
+		
+		$this->request->headers['Content-Type'] = File::mime_by_ext($file);
+		$this->request->headers['Content-length'] = filesize($file);
+		$this->request->headers['Last-Modified']  = date('r', filemtime($file));
+		
+		if (isset($cache))
+		{
+			$this->request->headers['Cache-Control'] = 'must-revalidate';
+			$this->request->headers['Expires'] = gmdate("D, d M Y H:i:s", time() + 86400).' GMT';
+		}
+		
 		$this->request->send_headers();
-
-		$image = @ fopen($this->file, 'rb');
+		
+		$image = @ fopen($file, 'rb');
 		if ($image)
 		{
 			fpassthru($image);
 			exit;
 		}
+	}
+	
+	public function gzip($file='')
+	{
+		$this->cached = APPPATH.'cache/resources/'.md5_file($file);
+		
+		if (!is_file($file))
+		{
+			throw new Kohana_Exception('File does not exist');
+		}
+		
+		$fh = fopen($this->cached, "w+");
+		fwrite($fh, gzencode(file_get_contents($file)));
+		fclose($fh);
+		
+		return $this->cached;
 	}
 
 } // End Controller_Resources
