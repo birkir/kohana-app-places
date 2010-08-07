591af5d2f40c8f2f1bba93e98728ef9631eea61c
Birkir R Gudjonsson <birkir@cube0.example.com>
2010-08-07 20:27:36 +0000

    Created Want controller

---
+++ classes/controller/want.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Want extends Controller_Interface {
+
+	/**
+	 * List all food types
+	**/
+	public function action_index()
+	{
+	}
+
+}
