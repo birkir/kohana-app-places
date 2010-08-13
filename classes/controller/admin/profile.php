52dd6a2715f368757310abaa54008232665d753b
Birkir R Gudjonsson <birkir.gudjonsson@gmail.com>
2010-08-13 12:18:11 +0000

    Added profile controller in admin

---
+++ classes/controller/admin/profile.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Admin_Profile controller
+ *
+ * @package    Eat.is
+ * @category   Admin
+ * @author     Birkir Rafn Gudjonsson
+ * @copyright  (c) 2010 Eat.is
+ */
+class Controller_Admin_Places extends Controller_Admin {
+	
+	public $title = "Profile";
+	
+	public function action_index()
+	{
+	}
+
+}
